bba4effbe43f2f00fe29c1aad4e5247eaf5826f7
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-10-03 23:52:03 +0200

    Update fish-prompt.fish: Add virtualenv, ruby version

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -7,15 +7,32 @@ function fish_prompt --description 'Write out the prompt'
 	end
 	
 	# Time
+	set_color 696969
   printf '\n[%s] ' (date "+%H:%M:%S")
+  set_color normal
+  # printf '\n'
 
 	# PWD
 	set_color $fish_color_cwd
 	echo -n (prompt_pwd)
+	
+	if set -q VIRTUAL_ENV
+    echo -n -s " " (set_color blue) "(python:" (basename "$VIRTUAL_ENV") ")" (set_color normal) ""
+	end
+	
+	set -l ruby_version (rbenv version-name 2>&1 /dev/null)
+	set -l ruby_status $status
+	if test $ruby_status -eq 0
+    switch $ruby_version;
+      case 'system';
+      case '*';
+      	echo -n -s " " (set_color red) "(ruby:" $ruby_version ")" (set_color normal) ""
+    end
+	end
+	
 	set_color normal
-	# set_color yellow
-
-	printf '%s ' (__fish_git_prompt)
+	echo -n -s (__fish_git_prompt) " "
+	set_color normal
 	
 	if not test $last_status -eq 0
 		set_color $fish_color_error
