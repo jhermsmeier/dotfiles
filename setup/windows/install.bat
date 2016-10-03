335af7094b78cb8f1d4fb16694a0e5507f00656f
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-10-04 00:42:25 +0200

    Add setup/windows from github/jhermsmeier/machine

---
+++ setup/windows/install.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: ENVIRONMENT
+:: git, clink, console2
+:: mingw32, mingw-installer (mingw64)
+
+:: RUNTIMES
+:: rust, node.js, ruby, python, perl
+
+:: DIVERSE
+:: ninite installer
+
+:: BROWSERS
+:: chrome, canary, firefox, nightly
+
+:: DEVTOOLS
+:: sublime text, brackets
+:: visual studio
+
+:: LOCAL LIB
+:: node-webkit, tcc, autoruns, cloc, procexp, wifikeys,
+:: whois, sigcheck, shmnview, ru, procdump, ...
---
+++ setup/windows/setup.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Downgrade UAC
+
+:: Symlink all files in home/ into %homepath%
+
+:: Download Clink and append injection to .cmdrc
+::  >> "%programfiles%\clink\0.4\clink.bat" inject
+
+:: Set up .ssh/
+
+:: Run post-install after MinGW install
+
+:: Add own packages (themes, settings) to Sublime Text %appdata%
+
+:: Register "purge" command for before shutdown,
+:: to keep the machine clean from .dll clutter
