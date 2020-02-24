96b254dc541dff08309499ca70a2b6b6799ba364
Jonas Hermsmeier <jhermsmeier@gmail.com>
2020-02-24 13:43:10 +0100

    fish: Update fish_prompt
    - Put prompt on new line
    - Change prompt char from '$' -> '>'

--- config/fish/.config/fish/functions/fish_prompt.fish
+++ config/fish/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 function fish_prompt --description 'Write out the prompt'
-	
+
 	set -l last_status $status
 
 	if not set -q __fish_prompt_normal
 		set -g __fish_prompt_normal (set_color normal)
 	end
-	
+
 	# Time
 	set_color 696969
   printf '\n[%s] ' (date "+%H:%M:%S")
@@ -15,20 +15,21 @@ function fish_prompt --description 'Write out the prompt'
 	# PWD
 	set_color $fish_color_cwd
 	echo -n (prompt_pwd)
-	
+
 	if set -q VIRTUAL_ENV
     echo -n -s " " (set_color blue) "(python:" (basename "$VIRTUAL_ENV") ")" (set_color normal) ""
 	end
-	
+
 	set_color normal
 	echo -n -s (__fish_git_prompt) " "
 	set_color normal
-	
+
 	if not test $last_status -eq 0
 		set_color $fish_color_error
 	end
 
-	echo -n '$ '
+  printf '\n'
+	echo -n ' > '
 	set_color normal
 
 end
